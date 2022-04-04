a162557550d53b30420bde230cf43eac0df2ffda
Ignatius <ignatiusrivaldolie@mhs.mdp.ac.id>
2022-04-04 10:01:44 +0700

    `Masukan gambar`

--- app/src/main/java/com/skripsi/steganografidhalgorithm/MainActivity.java
+++ app/src/main/java/com/skripsi/steganografidhalgorithm/MainActivity.java
@@ -1,13 +1,93 @@
 package com.skripsi.steganografidhalgorithm;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.view.View;
+import android.widget.ImageView;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.content.ContextCompat;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image=findViewById(R.id.ivCitraR);
+        image.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                boolean pick = true;
+                if (pick == true){
+                    if (!checkCameraPermission()){
+                        requestCameraPermission();
+
+                    }else PickImage();
+                }else{
+                    if (!checkStoragePermission()){
+                        requestStoragePermission();
+
+                    }else PickImage();
+                }
+            }
+        });
     }
+
+    private void PickImage() {
+        CropImage.activity().start(this);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestStoragePermission() {
+        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestCameraPermission() {
+        requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
+    }
+
+    private boolean checkStoragePermission(){
+        boolean perm2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED;
+        return perm2;
+    }
+    private boolean checkCameraPermission(){
+        boolean perm1 = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED;
+        boolean perm2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED;
+        return perm1 && perm2;
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                Picasso.with(this).load(resultUri).into(image);
+                /*try{
+                    InputStream stream = getContentResolver().openInputStream(resultUri);
+                    Bitmap bitmap = BitmapFactory.decodeStream(stream);
+                    image.setImageBitmap(bitmap);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }*/
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+
 }
