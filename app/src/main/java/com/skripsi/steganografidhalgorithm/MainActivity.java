ceb6bfca326d8579858bab1d0b831474e7476e10
Ignatius <ignatiusrivaldolie@mhs.mdp.ac.id>
2022-04-18 10:17:50 +0700

    `Steganografi Encode`

--- app/src/main/java/com/skripsi/steganografidhalgorithm/MainActivity.java
+++ app/src/main/java/com/skripsi/steganografidhalgorithm/MainActivity.java
@@ -1,69 +1,138 @@
 package com.skripsi.steganografidhalgorithm;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.core.content.ContextCompat;
-import com.squareup.picasso.Picasso;
+
+import com.ayush.imagesteganographylibrary.Text.AsyncTaskCallback.TextEncodingCallback;
+import com.ayush.imagesteganographylibrary.Text.ImageSteganography;
+import com.ayush.imagesteganographylibrary.Text.TextEncoding;
+
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
-import java.io.InputStream;
 
-public class MainActivity extends AppCompatActivity {
-    ImageView image;
+import java.io.*;
+
+public class MainActivity extends AppCompatActivity implements TextEncodingCallback {
+    private ImageView image;
+    private Button encode;
+    private Button decode;
+    private TextView publicKeyAnda;
+    private EditText publicKeyTeman;
+    private EditText secretMessage;
+
+    private TextEncoding textEncoding;
+    private ImageSteganography imageSteganography;
+    private ProgressDialog save;
+    private Uri filepath;
+
+    private Bitmap original_image;
+    private Bitmap encoded_image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        image=findViewById(R.id.ivCitraR);
+
+        setContentView(R.layout.activity_main); //activityMain
+        image=findViewById(R.id.ivCitraR); //ImageView Tengah
+        encode= findViewById(R.id.btEncode); //tombol encode
+        decode = findViewById(R.id.btDecode); //Tombol decode
+
+        publicKeyAnda = findViewById(R.id.tvpublicuser1); //public key kita
+        publicKeyTeman = findViewById(R.id.etPublic); //public key temen kita/lawan bicara
+        secretMessage = findViewById(R.id.etSecretMessage); //secret message/pesan rahasia
+
+        //Method Imageview kalau diclick
         image.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
                 boolean pick = true;
+                //Kondisi Jika bisa pick gambar maka cek permission camera
                 if (pick == true){
                     if (!checkCameraPermission()){
                         requestCameraPermission();
-
                     }else PickImage();
+                //Kondisi Jika tidak pick gambar, maka cek permission storage
                 }else{
                     if (!checkStoragePermission()){
                         requestStoragePermission();
-
                     }else PickImage();
                 }
             }
         });
+        encode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (filepath != null) {
+                    if (secretMessage.getText() != null) {
+                        //ImageSteganography Object instantiation
+                        imageSteganography = new ImageSteganography(secretMessage.getText().toString(),
+                                publicKeyTeman.getText().toString(),
+                                original_image);
+                        //TextEncoding object Instantiation
+                        textEncoding = new TextEncoding(encode.this, encode.this);
+                        //Executing the encoding
+                        textEncoding.execute(imageSteganography);
+                    }
+                }
+            }
+            final Bitmap imgToSave = encoded_image;
+            Thread PerformEncoding = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    saveToInternalStorage(imgToSave);
+                }
+            });
+            save = new ProgressDialog(encode.this);
+                save.setMessage("Saving, Please Wait...");
+                save.setTitle("Saving Image");
+                save.setIndeterminate(false);
+                save.setCancelable(false);
+                save.show();
+                PerformEncoding.start();
+        });
     }
 
+    //Ambil Image yang diinput
     private void PickImage() {
         CropImage.activity().start(this);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
+    //Request Storage
     private void requestStoragePermission() {
         requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
+    //Request Camera dan Storage
     private void requestCameraPermission() {
         requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
     }
 
+    //Check Request Storage
     private boolean checkStoragePermission(){
         boolean perm2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED;
         return perm2;
     }
+
+    //Check Request Storage dan Camera
     private boolean checkCameraPermission(){
         boolean perm1 = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED;
         boolean perm2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED;
@@ -76,18 +145,41 @@ public class MainActivity extends AppCompatActivity {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
                 Uri resultUri = result.getUri();
-                Picasso.with(this).load(resultUri).into(image);
-                /*try{
+                try{
                     InputStream stream = getContentResolver().openInputStream(resultUri);
                     Bitmap bitmap = BitmapFactory.decodeStream(stream);
                     image.setImageBitmap(bitmap);
                 }catch (Exception e){
                     e.printStackTrace();
-                }*/
+                }
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception error = result.getError();
             }
         }
     }
 
+    @Override
+    public void onStartTextEncoding() {
+
+    }
+
+    @Override
+    public void onCompleteTextEncoding(ImageSteganography imageSteganography) {
+
+    }
+    private void saveToInternalStorage(Bitmap bitmapImage) {
+        OutputStream fOut;
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS), "Encoded" + ".PNG"); // the File to save ,
+        try {
+            fOut = new FileOutputStream(file);
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fOut); // saving the Bitmap to a file
+            fOut.flush(); // Not really required
+            fOut.close(); // do not forget to close the stream
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
